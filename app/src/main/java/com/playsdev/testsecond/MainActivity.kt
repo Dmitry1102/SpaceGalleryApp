bc8674afd24fdc260d093981ef7d940dd1baff2d
Dmitry1102 <meleshkevich2016@mail.ru>
2022-02-17 09:14:03 +0100

    first commit

---
+++ app/src/main/java/com/playsdev/testsecond/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.playsdev.testsecond
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+    }
+}
